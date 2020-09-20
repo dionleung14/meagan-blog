7abb62f97afcac01a0bad777495065716dcb31eb
NA Flipper <dioncleung@gmail.com>
2020-09-20 01:28:39 -0700

    finalized display of blogs on home page

--- src/components/IndividBlog.js
+++ src/components/IndividBlog.js
@@ -8,16 +8,18 @@ export default function IndividBlog(props) {
   let dateArr = date.split(" ");
 
   // get only ["Sep", "17", "2020"]
-  let calDate = dateArr.slice(1, 3);
+  let calDate = dateArr.slice(1, 4);
 
-  // get time created
+  // get time created as ["11:01:26"]
   let time = dateArr.slice(4, 5);
+
+  console.log(dateArr);
   return (
     <div className="bg-gray-600 w-full my-4 px-2 py-4 flex flex-row justify-evenly">
       <div>
         <h1 className="text-2xl bg-red-100">{calDate.join(" ")}</h1>
         <h1 className="text-lg bg-red-400">time: {time}</h1>
-        <h1 className="text-lg bg-red-400">Title: {props.title}</h1>
+        <h1 className="text-lg bg-red-400 text-center">Title: {props.title}</h1>
         <h1 className="text-lg bg-red-900">Body: {props.body}</h1>
       </div>
       <img className="" src="http://placeimg.com/240/240/any" alt="blog 1" />
