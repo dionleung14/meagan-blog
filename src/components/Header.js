8fd69d85bdc4aff66d6c9287be0b9e0763e8b97b
NA Flipper <dioncleung@gmail.com>
2020-09-27 23:59:57 -0700

    troubleshooting position of navbar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Header extends Component {
   render() {
     return (
-      <h1 className="w-full my-8 text-2xl text-center italic h-24 top-0">
+      <h1 className="w-full text-2xl text-center italic h-24 fixed top-0 bg-green-200">
         Maraki Minimalism
       </h1>
     );
