aeef80cb2eefa6c515be66d52de85802de41b723
NA Flipper <dioncleung@gmail.com>
2020-10-13 13:58:06 -0700

    added more styling to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        <h1 className="w-full text-2xl text-center italic h-32 fixed top-0 bg-green-200">
+        <h1 className="w-full text-2xl text-center italic h-32 fixed top-0 bg-green-200 z-20">
           Maraki Minimalism
           <Navbar logged={true} />
         </h1>
